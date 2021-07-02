542348cc7b04332931003ddac8dd73a1fef28fe6
Ruel Neuman <ruelneuman@gmail.com>
2021-07-02 14:33:11 -0700

    create initital version of repositories list as per ex. 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View >
+      <Text>Full Name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
