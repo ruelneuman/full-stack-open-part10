971f64256a0d22f12fadda0c7342ddb7e0360587
Ruel Neuman <ruelneuman@gmail.com>
2021-07-08 11:27:22 -0700

    add utility function auth storage as per ex. 10.14

---
+++ src/utils/authStorage.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+    this.tokenKey = `${this.namespace}:token`;
+  }
+
+  async getAccessToken() {
+    return await AsyncStorage.getItem(this.tokenKey);
+  }
+
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(this.tokenKey, accessToken,);
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(this.tokenKey);
+  }
+}
+
+export default AuthStorage;
